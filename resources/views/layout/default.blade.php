75f2202ac37d7751aa78d0115773dae89a126784
Najbudin007 <najbudinalam55@gmail.com>
2025-01-22 19:00:53 +0545

    APP::Added Some minor Changes into defaultPages

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -61,7 +61,6 @@
         </div>
         @yield('content')
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
